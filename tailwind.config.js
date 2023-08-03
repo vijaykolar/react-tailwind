548fb4072ce19513f341b0c0379d814144a4336a
Vijay Kolar <vijayikolar@gmail.com>
2023-08-03 10:22:13 +0530

    Fixed issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
